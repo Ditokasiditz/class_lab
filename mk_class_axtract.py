4cad4b427ddeed78ed7359f8cc142932852a2c32
Ditokasiditz <kasidit.samad@gmail.com>
2023-02-09 00:10:35 +0700

    Update mk_class_axtract.py

--- mk_class_axtract.py
+++ mk_class_axtract.py
@@ -10,10 +10,17 @@ class Product:
 class Cart_Item:
     def __init__(self, Quantity, Discount,Name, ImageURL, Price ):
         self._Quantity = Quantity
-        self.Discount = Discount
-        self._Name = Name
+        self._Discount = Discount
+        self._Name = Name                   #from class product
         self._ImageURL = ImageURL
         self._Price = Price
 
 
-
+class Cart:
+    def __init__(self, Shipping_Cost , Total_Cost, Cart_Item,Quantity, Discount, Price):
+        self._Shipping_Cost = Shipping_Cost
+        self._Total_Cost = Total_Cost
+        self._Cart_Item = []               #list of item in cart 
+        self._Quantity = Quantity
+        self._Discount = Discount
+        self._Price = Price
