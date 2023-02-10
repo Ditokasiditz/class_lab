1f91559036b380f072b4f5e593cdaf78bed981d2
Ditokasiditz <kasidit.samad@gmail.com>
2023-02-10 22:16:18 +0700

    update user and order

--- mk_class_axtract.py
+++ mk_class_axtract.py
@@ -56,19 +56,21 @@ class Account:
 
 
 class User:
-    def __init__(self, Id, Password, Status, Account):
+    def __init__(self, Id, Password, Status, Account, Order):
         self._Id = Id
         self._Password = Password
         self._Status = Status
         self._Account = Account             #list of Account
+        self._Order = []                    #list of Order
 
 
 class Order:
-    def __init__(self, Shipping_Address, Total_Cost, Status, Payment_Status):
+    def __init__(self, Shipping_Address, Total_Cost, Status, Payment_Status, User_Id):
         self._Shipping_Address = Shipping_Address
         self._Total_Cost = Total_Cost
         self._Status = Status
         self._Payment_Status = Payment_Status           #from clss payment
+        self._User_Id = User_Id                         #from User
 
 
 class Payment:
